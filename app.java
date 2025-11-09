ac5d2235ce41d305effc9cbbf09741e41dac95d6
yadlaramadevi7 <yadlaramadevi7@gmail.com>
2025-11-09 12:14:06 +0530

    Update app.java

---
+++ app.java
@@ -0,0 +1,19 @@
+// HelloWorld.java
+
+/**
+ * The HelloWorld class implements an application that
+ * simply prints "Hello, World!" to the standard output.
+ */
+public class HelloWorld {
+
+    /**
+     * The main method is the entry point of the Java application.
+     * It is executed when the program is run.
+     *
+     * @param args Command line arguments (not used in this example).
+     */
+    public static void main(String[] args) {
+        // Prints the string "Hello, World!" to the console.
+        System.out.println("Hello, World!");
+    }
+}
